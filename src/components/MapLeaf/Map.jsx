723c26455f7ed44a6027db85e19a135e1ee388e9
lpleinte <louispleintel@gmail.com>
2025-03-28 17:34:01 +0100

    icone map

--- src/components/MapLeaf/Map.jsx
+++ src/components/MapLeaf/Map.jsx
@@ -8,6 +8,13 @@ import 'leaflet/dist/leaflet.css';
 const Map = () => {
   const positionsCity = [49.441620, 1.090726];
 
+  const customIcon = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png', 
+    iconSize: [25, 41], // Taille de l'icône
+    iconAnchor: [12, 41], // Ancrage de l'icône
+    popupAnchor: [1, -34], // Ancrage de la popup
+  });
+
   return (
     <div style={{ width: '100%', height: '400px' }}>
     <MapContainer center={positionsCity} zoom={13} scrollWheelZoom={false}>
@@ -15,7 +22,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={positionsCity}>
+      <Marker position={positionsCity} icon={customIcon}>
         <Popup>
           {userDatas.city}
         </Popup>
